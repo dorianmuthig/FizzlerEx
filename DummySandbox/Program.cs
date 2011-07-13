5f9845bade861b2dd9c4a98e6731544f9ea1076c
antiufo <antiufo@localhost>
2011-07-13 12:27:28 +0200

    development sandbox

---
+++ DummySandbox/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+using Fizzler.Systems.HtmlAgilityPack;
+
+namespace DummySandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var k = HtmlNode.CreateNode(@"
+
+<span><span>
+<span><b></b><span>
+
+
+");
+
+            var r = k.QuerySelector("span:has(b)");
+
+        }
+    }
+}

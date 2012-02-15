144e1e805ded6692ad74ac79d17ab03ae5ba4685
antiufo <antiufo@gmail.com>
2012-02-15 11:35:16 +0100

    started work on CachingDictionary, imported PriorityQueue

---
+++ Fizzler/CachingDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fizzler
+{
+    internal class CachingDictionary<TInput, TResult>
+    {
+        public CachingDictionary(Func<TInput, TResult> evalutor)
+            : this(evalutor, 30)
+        {
+        }
+
+        public CachingDictionary(Func<TInput, TResult> evalutor, int capacity)
+        {
+            this.capacity = capacity;
+        }
+
+        private int capacity;
+        private Dictionary<TInput, TResult> dictionary = new Dictionary<TInput, TResult>();
+        private Queue<TInput> queue = new Queue<TInput>();
+
+
+        //public TResult GetValue(TInput input)
+        //{
+        //    TResult result;
+        //    if (dictionary.TryGetValue(input, out result))
+        //    {
+        //        return result;
+        //    }
+        //}
+
+
+    }
+}

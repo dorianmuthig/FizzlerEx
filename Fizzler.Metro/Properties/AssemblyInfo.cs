6dd089cade13dadb34b4c4b3dfbf37b85665de2b
antiufo <antiufo@gmail.com>
2012-03-27 10:10:26 +0200

    added "Metro version" in assembly description

--- Fizzler.Metro/Properties/AssemblyInfo.cs
+++ Fizzler.Metro/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("FizzlerEx")]
-[assembly: AssemblyDescription("CSS Selector Engine for .NET")]
+[assembly: AssemblyDescription("CSS Selector Engine for .NET (Metro version)")]

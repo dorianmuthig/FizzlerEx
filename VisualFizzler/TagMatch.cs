3f5951ffd6810b12457fbcb7948637a48c6bdfac
antiufo <antiufo@gmail.com>
2012-04-09 11:17:59 +0200

    fixed imprecise highlighting

---
+++ VisualFizzler/TagMatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VisualFizzler
+{
+    public struct TagMatch
+    {
+        private int _start;
+        private int _length;
+
+        public TagMatch(int start, int length)
+        {
+            this._start = start;
+            this._length = length;
+        }
+
+        public int Index { get { return _start; } }
+        public int Length { get { return _length; } }
+    }
+}
